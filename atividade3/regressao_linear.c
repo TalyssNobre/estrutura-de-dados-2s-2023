14cd8f90aa5cd8c53f14321483e98cb61a59b29d
TalyssNobre <talysalexandrenobre2004@gmail.com>
2023-09-03 21:33:10 -0300

    subir dnv

--- atividade3/regressao_linear.c
+++ atividade3/regressao_linear.c
@@ -1,83 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 #include <string.h>
+#include <math.h>
 
+#define MAX_TAM_REG_DADOS 15
+#define TAM_AUX 10
 
-struct Ponto {
-    double x;
-    double y;
-};
+typedef struct Ponto{
+    int x;
+    float y;
+} Ponto;
 
-int main() {
-printf("Abrindo o arquivo...\n");
-FILE *arquivo = fopen("atividade3/dados.csv", "r");
+void converter_string_para_numero(FILE * fptr, Ponto * ptr_pontos, char * reg_dados, int qtd_regs);
+    void remover_lixo_aux(char * aux_x, char * aux_y);
+void calcular_medias_x_y(Ponto * ptr_pontos, int qtd_regs, float * media_x, float * media_y);
+void calcular_inclinacao(Ponto * ptr_pontos, int qtd_regs, float media_x, float media_y, float * inclinacao);
+void calcular_intercepcao(float media_x, float media_y, float inclinacao, float * intercecao);
 
+int main(int argc, char *argv[]){
+    FILE *fptr = NULL;
+    char reg_dados[MAX_TAM_REG_DADOS];
+    int qtd_regs = 0;
+    Ponto * ptr_pontos; 
 
-if (!arquivo) {
-    perror("Erro ao abrir o arquivo, tente novamente");
-    return 1;
-}
-printf("Arquivo aberto com sucesso.\n");
+    float media_x = 0.0f, media_y = 0.0f;
+    float inclinacao = 0.0f;
+    float intercepcao = 0.0f;
+    
 
     
-    int numLinhas = 0;
-    char linha[1024]; 
-    while (fgets(linha, sizeof(linha), arquivo)) {
-        numLinhas++;
+    fptr = fopen(argv[1],"r");
+    while(fgets(reg_dados, MAX_TAM_REG_DADOS, fptr)){
+        qtd_regs++;
     }
-
-  
-    struct Ponto *pontos = (struct Ponto *)malloc(numLinhas * sizeof(struct Ponto));
-    if (!pontos) {
-        perror("Erro ao alocar a memória");
-        fclose(arquivo);
-        return 1;
-    }
-
-
-    rewind(arquivo);
-
-   
-    int i = 0;
-    while (fgets(linha, sizeof(linha), arquivo)) {
-      
-        char *token = strtok(linha, ",");
-        pontos[i].x = atof(token);
-        token = strtok(NULL, ",");
-        pontos[i].y = atof(token);
-        i++;
-    }
-
-    fclose(arquivo);
-
-   
-    double somaX = 0.0, somaY = 0.0;
-    for (int j = 0; j < numLinhas; j++) {
-        somaX += pontos[j].x;
-        somaY += pontos[j].y;
-    }
-
-    double mediaX = somaX / numLinhas;
-    double mediaY = somaY / numLinhas;
-
-   
-    double somaXY = 0.0, somaXQuadrado = 0.0;
-    for (int j = 0; j < numLinhas; j++) {
-        somaXY += (pontos[j].x - mediaX) * (pontos[j].y - mediaY);
-        somaXQuadrado += (pontos[j].x - mediaX) * (pontos[j].x - mediaX);
-    }
-
-    double a = somaXY / somaXQuadrado;
-    double b = mediaY - (a * mediaX);
-
-   
-    printf("Resultados da Regressão Linear:\n");
-    printf("Equação da Regressão Linear: y = %.2fx + %.2f\n", a, b);
-    printf("Coeficiente Angular (Inclinação): a = %.2f\n", a);
-    printf("Coeficiente Linear (Ponto de Intercepção): b = %.2f\n", b);
+    fclose(fptr);
 
     
-    free(pontos);
+    ptr_pontos = malloc(sizeof(Ponto) * qtd_regs);
 
+
+    fptr = fopen(argv[1],"r");
+    converter_string_para_numero(fptr, ptr_pontos, reg_dados, qtd_regs);
+    fclose(fptr);
+
+    
+    calcular_medias_x_y(ptr_pontos, qtd_regs, &media_x, &media_y);
+
+    
+    calcular_inclinacao(ptr_pontos, qtd_regs, media_x, media_y, &inclinacao);
+
+    
+    calcular_intercepcao(media_x, media_y, inclinacao, &intercepcao);
+
+    //SAÍDA
+    
+    printf("FORMULA DA REGRESSAO LINEAR (dados.csv)\n\n");
+    printf("\ty = %.1fx + %.0f", inclinacao, intercepcao);
+    getch();
+
+    //desalocar vetor de structs (pontos)
+    free(ptr_pontos);
+    
     return 0;
+}
+
+void converter_string_para_numero(FILE * fptr, Ponto * ptr_pontos, char * reg_dados, int qtd_regs){
+    int i = 0, j = 0, k = 0;
+    char aux_x[TAM_AUX], aux_y[TAM_AUX];
+
+    //atribuição dos valores x e y para cada ponto
+    for(i = 0; i < qtd_regs; i++){
+        fgets(reg_dados, MAX_TAM_REG_DADOS, fptr);
+
+        //dividir registro nos valores x e y
+        for(j = 0; reg_dados[j] != ','; j++){
+            aux_x[j] = reg_dados[j];
+        }
+        for(k = 0, j++; j < strlen(reg_dados) - 1; k++,j++){
+            aux_y[k] = reg_dados[j];
+        }
+
+        //conversão e atribuição dos valores x e y
+        ptr_pontos[i].x = atoi(aux_x);
+        ptr_pontos[i].y = atof(aux_y);
+
+        //remover o lixo das strings auxiliares
+        remover_lixo_aux(&aux_x[0], &aux_y[0]);
+    }    
+}
+
+    void remover_lixo_aux(char * aux_x, char * aux_y) {
+        int i = 0;
+
+        for(i; i < TAM_AUX; i++){
+            aux_x[i] = '\0';
+            aux_y[i] = '\0';
+        }    
+    }
+
+void calcular_medias_x_y(Ponto * ptr_pontos, int qtd_regs, float * media_x, float * media_y){
+    int soma_x = 0;
+    float soma_y = 0.0f;
+    int i = 0;
+
+    for(i; i < qtd_regs; i++){
+        soma_x += ptr_pontos[i].x;
+        soma_y += ptr_pontos[i].y;
+    }
+
+    *media_x = (float)soma_x / (float)qtd_regs;
+    *media_y =        soma_y / (float)qtd_regs;
+}
+
+void calcular_inclinacao(Ponto * ptr_pontos, int qtd_regs, float media_x, float media_y, float * inclinacao) {
+    float soma_numerador = 0.0f, soma_denominador = 0.0f;
+    int i = 0;
+
+    for(i; i < qtd_regs; i++){
+        soma_numerador += (float)(ptr_pontos[i].x - media_x) * (ptr_pontos[i].y - media_y); //somatório numerador
+        soma_denominador += pow((float)(ptr_pontos[i].x - media_x), 2);                     //somatório denominador
+    }
+
+    //inclinação
+    *inclinacao = soma_numerador / soma_denominador;
+}
+
+void calcular_intercepcao(float media_x, float media_y, float inclinacao, float * intercepcao) {
+    //intercepcao
+    *intercepcao = media_y - (inclinacao * media_x);
 }
